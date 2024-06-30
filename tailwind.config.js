23461d14c5e07b6b5cb7d44e40267cb78fc969e5
TyrusYeo <tyrusyeo5@gmail.com>
2024-06-30 15:30:21 -0400

    Hello world

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'pantone-orange': '#FF8200',
+        'pantone-green': '#4CAF50',
+        'pantone-blue': '#00B0CA',
+      }
     },
   },
   plugins: [],
